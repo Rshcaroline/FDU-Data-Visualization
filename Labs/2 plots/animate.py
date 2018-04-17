354b0dc5cae707a3ec5d0709ba068fcf51c2b90e
Rshcaroline <rshcaroline@163.com>
2018-04-17 21:58:12 +0800

    hw2

--- Labs/2 plots/animate.py
+++ Labs/2 plots/animate.py
@@ -19,11 +19,13 @@ ax.set_ylim(min(spiScore), max(spiScore))
 ax.set_xlim(0, len(x_pos))
 line, = ax.plot([], [], lw=1)
 xdata, ydata = [],[]
+
 def animate(i):
     print ("append_line ",i)
     ax.annotate(countryList[i] + ":" + str(spiScore[i]), (i, spiScore[i] ))
     line.set_data(x_pos[0:i+1], spiScore[0:i+1])
     return line
 
-anim = animation.FuncAnimation(fig,animate,len(x_pos), interval=100,blit=False)
+anim = animation.FuncAnimation(fig, animate,len(x_pos), interval=10, blit=False)
+anim.save('animation.mp4', writer='ffmpeg', fps=30)
 plt.show()
