1774a6524620427491dc9382e85cf84f3964c6c2
Rshcaroline <rshcaroline@163.com>
2018-05-19 19:42:19 +0800

    hw4

--- HW/HW4/Q3.py
+++ HW/HW4/Q3.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.misc import imread, imshow
 
 
-############################
+# Interpolation
 def GetBilinearPixel(imArr, posX, posY):
     out = []
     # Get integer and fractional parts of numbers
@@ -31,145 +31,145 @@ def GetBilinearPixel(imArr, posX, posY):
 
 
 if __name__ == "__main__":
+    fold = './data/'
     im = imread(fold + "feifei.png", mode="RGB")
-    enlargedShape = list(map(int, [im.shape[0] * 4, im.shape[1] * 4, im.shape[2]]))
+
+    enlargedShape = [int(im.shape[0] * 4), int(im.shape[1] * 4), im.shape[2]]
     enlargedImg = np.empty(enlargedShape, dtype=np.uint8)
     rowScale = float(im.shape[0]) / float(enlargedImg.shape[0])
     colScale = float(im.shape[1]) / float(enlargedImg.shape[1])
+
     for r in range(enlargedImg.shape[0]):
         for c in range(enlargedImg.shape[1]):
-            orir = r * rowScale  # Find position in original image
             oric = c * colScale
+            orir = r * rowScale  # Find position in original image
             enlargedImg[r, c] = GetBilinearPixel(im, oric, orir)
-    print("interploation is completed")
+    print("Hey, Interploation is completed")
 
-############################
-# 读取灰度图像并获得矩阵长宽
-fold = './data/'
-image = Image.open(fold + 'feifei.png').convert("L")
-image_array = np.array(image)
-k = len(enlargedImg)
-i, j = enlargedImg[0].shape
+    ############################
+    # 读取灰度图像并获得矩阵长宽
+    fold = './data/'
+    image = Image.open(fold + 'feifei.png').convert("L")
+    image_array = np.array(image)
+    k = len(enlargedImg)
+    i, j = enlargedImg[0].shape
 
-T = {0: [], 1: [], 2: []}
+    T = {0: [], 1: [], 2: []}
 
-############################
-im_new_array = enlargedImg
-# 循环遍历i,j
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    ############################
+    im_new_array = enlargedImg
+    # 循环遍历i,j
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 240:
                     im_new_array[c][a][b] = 0
-print("240's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("240's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 224:
                     im_new_array[c][a][b] = 0
-print("224's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("224's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 208:
                     im_new_array[c][a][b] = 0
-print("208's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("208's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 192:
                     im_new_array[c][a][b] = 0
-print("192's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("192's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 176:
                     im_new_array[c][a][b] = 0
-print("176's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("176's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 160:
                     im_new_array[c][a][b] = 0
-print("160's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("160's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 128:
                     im_new_array[c][a][b] = 0
-print("128's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("128's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 96:
                     im_new_array[c][a][b] = 0
-print("96's value is completed")
+    print("96's value is completed")
 
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 80:
                     im_new_array[c][a][b] = 0
-print("80's value is completed")
+    print("80's value is completed")
 
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 64:
                     im_new_array[c][a][b] = 0
-print("64's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("64's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 48:
                     im_new_array[c][a][b] = 0
-print("48's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("48's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 32:
                     im_new_array[c][a][b] = 0
-print("32's value is completed")
-############################
-for c in range(k):
-    for a in range(i):
-        for b in range(j):
-            for t in range(3):
+    print("32's value is completed")
+
+    ############################
+    for c in range(k):
+        for a in range(i):
+            for b in range(j):
                 if enlargedImg[c][a][b] == 16:
                     im_new_array[c][a][b] = 0
-print("16's value is completed")
+    print("16's value is completed")
 
-plt.imshow(im_new_array, cmap='gray')
-plt.axis("off")
-plt.show()
+    plt.imshow(im_new_array, cmap='gray')
+    plt.axis("off")
+    plt.show()
 
-figure()
-gray()
-contour(image, origin='image')
-axis('equal')
-axis('off')
-title(u'图像轮廓')
-show()
+    # figure()
+    # gray()
+    # contour(image, origin='image')
+    # axis('equal')
+    # axis('off')
+    # title('what')
+    # show()
--- HW/HW4/Q4.py
+++ HW/HW4/Q4.py
@@ -1,31 +1,48 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-fold='./data/'
-img=Image.open(fold+'feifei.png')
-gray=np.array(img.convert('L')) #转换成灰度的Image格式和数组格式
+
+
+fold = './data/'
+img = Image.open(fold+'feifei.png')
+gray = np.array(img.convert('L'))      # 转换成灰度的Image格式和数组格式
 xsize, ysize = gray.shape
-resultimage=np.zeros([xsize,ysize],np.float)
-N=3#卷积核的层数
-# kern = np.ones([N,N],np.float)
-#kern=kern*1.0/(N*N)
+resultimage=np.zeros([xsize,ysize], np.float)
+N=3   # 卷积核的层数
+# kern = np.ones([N,N],np.float)s
+# kern = kern*1.0/(N*N)
+
+# kern = np.mat([[0,-1,0],[-1,4,-1],[0,-1,0]])
+# kern = np.mat([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
 kern = np.mat([[-0.8,-0.8,-0.8],[-0.8,200,-0.8],[-0.8,-0.8,-0.8]])
-print(kern)
-ntime=1#?
-imgsm=gray
+ntime = 1
+imgsm = gray
+
 for nt in range(ntime):
-    for x in range(1,xsize-1):
-        for y in range(1,ysize-1):
-            smooth=0.0
-            for a in range(-1,2):
-                for b in range(-1,2):
-                    smooth+=imgsm[x+a,y+b]*kern[a+1,b+1]
-                resultimage[x,y]=smooth
-    imgsm=resultimage
+    for x in range(1, xsize-1):
+        for y in range(1, ysize-1):
+            smooth = 0.0
+            for a in range(-1, 2):
+                for b in range(-1, 2):
+                    smooth += imgsm[x+a,y+b] * kern[a+1,b+1]
+                resultimage[x,y] = smooth
+    imgsm = resultimage
 
 plt.figure('smooth'+str(ntime)+'times')
-plt.subplot(2,1,1),plt.title('origin')
-plt.imshow(gray, cmap='gray'),plt.axis('off')
-plt.subplot(2,1,2),plt.title('sharpen')
-plt.imshow(resultimage,cmap='gray'),plt.axis('off')
-plt.show()
+plt.subplot(3,1,1)
+plt.title('origin')
+plt.imshow(gray, cmap='gray')
+plt.axis('off')
+
+plt.subplot(3,1,2)
+plt.title('sharpen')
+plt.imshow(resultimage, cmap='gray')
+plt.axis('off')
+
+plt.subplot(3,1,3)
+plt.title('both')
+plt.imshow(resultimage+gray, cmap='gray')
+plt.axis('off')
+
+# plt.show()
+plt.savefig('Q4.png')
